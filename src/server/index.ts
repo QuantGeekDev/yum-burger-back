2168b55daa44f7da30bb7dfc997df7a72628d983
Alex Andru <alex007d@gmail.com>
2023-11-29 11:45:35 +0100

    Add customError middleware to middleware chain

--- src/server/index.ts
+++ src/server/index.ts
@@ -3,6 +3,7 @@ import chalk from "chalk";
 import app from "./app.js";
 import cors, { type CorsOptions } from "cors";
 import morgan from "morgan";
+import { errorMiddleware } from "./middlewares/errorMiddleware.js";
 
 const debug = debugCreator("server:");
 
@@ -12,6 +13,7 @@ const corsOptions: CorsOptions = {
 
 debug(chalk.blue("Initializing middlewares"));
 
+app.use(cors(corsOptions));
 app.use(morgan("dev"));
 
-app.use(cors(corsOptions));
+app.use(errorMiddleware);
