b6b43b4248dc017578c22fb53672de54f1d40fbe
Alex Andru <alex007d@gmail.com>
2023-11-29 11:09:39 +0100

    Add morgan middleware

--- src/server/index.ts
+++ src/server/index.ts
@@ -1,14 +1,17 @@
 import debugCreator from "debug";
 import chalk from "chalk";
-import app from "./app";
+import app from "./app.js";
 import cors, { type CorsOptions } from "cors";
+import morgan from "morgan";
 
 const debug = debugCreator("server:");
-
-debug(chalk.blue("Initializing middlewares"));
 
 const corsOptions: CorsOptions = {
   origin: ["https://alex-andru-202309-bcn-front.netlify.app/", "localhost:*"],
 };
+
+debug(chalk.blue("Initializing middlewares"));
+
+app.use(morgan("dev"));
 
 app.use(cors(corsOptions));
