158b463836cf886b673b473fa7aaa373b1fb8435
Alex Andru <alex007d@gmail.com>
2023-12-13 09:46:33 +0100

    Add editBurger to BurgerController

--- src/app/features/burger/repository/BurgerMongooseRepository/types.ts
+++ src/app/features/burger/repository/BurgerMongooseRepository/types.ts
@@ -1,10 +1,16 @@
-import { type BurgerStructure } from "../../types.js";
+import {
+  type BurgerFromMongooseStructure,
+  type BurgerStructure,
+} from "../../types.js";
 
 export interface BurgerRepository {
   getBurgers: (options?: BurgerRepositoryOptions) => Promise<BurgerStructure[]>;
   deleteBurger: (id: string) => Promise<BurgerStructure>;
   addBurger: (burger: BurgerStructure) => Promise<BurgerStructure>;
   getBurgerById: (id: string) => Promise<BurgerStructure>;
+  editBurger: (
+    burger: BurgerFromMongooseStructure,
+  ) => Promise<BurgerFromMongooseStructure>;
 }
 
 export interface BurgerRepositoryOptions {
