6b402813bd4fa252249cc4cfab3f93015cfa7fbb
Alex Andru <alex007d@gmail.com>
2024-01-07 22:46:15 +0100

    Add error tests to registerUserEndpoint test

--- src/app/features/user/mocks/userMockData.ts
+++ src/app/features/user/mocks/userMockData.ts
@@ -4,3 +4,16 @@ export const testUserMock = {
   password: "testPassword",
   isAdmin: true,
 };
+
+export const testUserMock2 = {
+  name: "John",
+  email: "testuser2@gmail.com",
+  password: "testPassword",
+  isAdmin: true,
+};
+export const testUserMock3 = {
+  name: "John",
+  email: "testuser3@gmail.com",
+  password: "testPassword",
+  isAdmin: true,
+};
