a00a195b64118cf20a2bd126a1dead5bf6c30798
Alex Andru <alex007d@gmail.com>
2024-01-07 16:07:13 +0100

    Add isAdmin flag to User model

--- src/app/features/user/model/User.ts
+++ src/app/features/user/model/User.ts
@@ -4,6 +4,7 @@ const userSchema = new Schema({
   name: { type: String, require: true },
   email: { type: String, require: true },
   password: { type: String, require: true },
+  isAdmin: { type: Boolean, default: false },
 });
 
 const User = mongoose.model("User", userSchema, "users");
